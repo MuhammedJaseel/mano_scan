eb48e16f96290073abf1489a3dd6ed6a6983b1ad
MuhammedJaseel <jaseelmanamulli@gmail.com>
2025-12-21 15:48:05 +0400

    OP

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,17 @@ import axios from "axios";
 import { ethers } from "ethers";
 import "./App.css";
 
+function showHex(hash: string) {
+  if (!hash) return "";
+  return (
+    <span className="text-[#63beed] font-bold">
+      {hash.slice(0, 12) + " ... " + hash.slice(-12)}
+    </span>
+  );
+}
+
 function App() {
-  const [wallets, setwallets] = useState({ totel: 0, data: [], busy: false });
+  const [accounts, setaccounts] = useState({ totel: 0, data: [], busy: false });
   const [transactions, settransactions] = useState({
     totel: 0,
     data: [],
@@ -21,8 +30,8 @@ function App() {
   const apiBase = "https://manoscan-api.vercel.app/api/";
   const loadData = async () => {
     try {
-      axios.get(apiBase + "wallets").then((res) => {
-        setwallets({ ...res.data, busy: false });
+      axios.get(apiBase + "accounts").then((res) => {
+        setaccounts({ ...res.data, busy: false });
       });
       axios.get(apiBase + "transactions").then((res) => {
         settransactions({ ...res.data, busy: false });
@@ -36,72 +45,60 @@ function App() {
   };
 
   return (
-    <div
-      style={{
-        width: "100vw",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "flex-start",
-        padding: 20,
-        boxSizing: "border-box",
-        gap: 20,
-        flexWrap: "wrap",
-      }}
-    >
-      <div style={{ minWidth: 300 }}>
-        <h2>Latest Wallets {wallets.busy ? "Loading..." : ""}</h2>
-        {wallets.data.map((it: any, k) => (
+    <div className="w-screen flex justify-center items-start p-3 gap-3 flex-wrap">
+      <div className="w-full max-w-100">
+        <div className="text-[28px] font-bold p-1">
+          Latest accounts {accounts.busy ? "Loading..." : ""}
+        </div>
+        {accounts.data.map((it: any, k) => (
           <div
             key={k}
-            style={{
-              border: "1px solid gray",
-              margin: 5,
-              padding: 5,
-              fontSize: 14,
-            }}
+            className="border border-gray-400 m-1 p-2 text-sm rounded"
           >
-            <div>Address: {it?.a}</div>
-            <div style={{ fontWeight: "bold" }}>
-              Balance: {ethers.formatEther(it?.b || "0n")}
+            <div>Address: {showHex(it?.a)}</div>
+            <div>
+              Balance:&nbsp;
+              <span className="text-[#a0aeco]">
+                {/* {Number(ethers.formatEther(it?.b || "0n")).toLocaleString()}{" "} */}
+                {ethers.formatEther(it?.b || "0n")}&nbsp;MANO
+              </span>
+            </div>
+            <div>
+              Txn Count: <span className="text-[#63beed]">{it?.n}</span>
             </div>
           </div>
         ))}
       </div>
-      <div style={{ minWidth: 300 }}>
-        <h2>Latest transactions {transactions.busy ? "Loading..." : ""}</h2>
+      <div className="w-full max-w-100">
+        <div className="text-[28px] font-bold p-1">
+          Latest transactions {transactions.busy ? "Loading..." : ""}
+        </div>
         {transactions.data.map((it: any, k) => (
           <div
             key={k}
-            style={{
-              border: "1px solid gray",
-              margin: 5,
-              padding: 5,
-              fontSize: 14,
-            }}
+            className="border border-gray-400 m-1 p-2 text-sm rounded"
           >
-            <div>Hash: {it?.th}</div>
-            <div>From: {it?.f}</div>
-            <div>To: {it?.t}</div>
-            <div>Value: {ethers.formatEther(it?.v || "0n")}</div>
+            <div>Hash: {showHex(it?.th)}</div>
+            <div>From: {showHex(it?.f)}</div>
+            <div>To: {showHex(it?.t)}</div>
+            <div>Value: {ethers.formatEther(it?.v || "0n")}&nbsp;MANO</div>
             <div>Estimat Gas: {ethers.formatEther(it?.gu || "0n")}</div>
           </div>
         ))}
       </div>
-      <div style={{ minWidth: 300 }}>
-        <h2>Latest blocks {blocks.busy ? "Loading..." : ""}</h2>
+      <div className="w-full max-w-100">
+        <div className="text-[28px] font-bold p-1">
+          Latest blocks {blocks.busy ? "Loading..." : ""}
+        </div>
         {blocks.data.map((it: any, k) => (
           <div
             key={k}
-            style={{
-              border: "1px solid gray",
-              margin: 5,
-              padding: 5,
-              fontSize: 14,
-            }}
+            className="border border-gray-400 m-1 p-2 text-sm rounded"
           >
             <div>Block Number: {it?.bn}</div>
-            <div>Block Hash: {it?.bh}</div>
-            {/* <div>Balance: {ethers.formatEther(it?.b)}</div> */}
+            <div>Block Hash: {showHex(it?.bh)}</div>
+            <div>Parent Hash: {showHex(it?.ph)}</div>
+            <div>Balance: {ethers.formatEther(it?.bu || "0x0")}</div>
           </div>
         ))}
       </div>
